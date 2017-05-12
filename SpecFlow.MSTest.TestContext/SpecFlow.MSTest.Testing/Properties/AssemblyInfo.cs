0b1472871c95471255449012da58ebff4b37c97e
Joe Cisar <jncqm2@gmail.com>
2017-05-12 15:58:08 -0500

    initial commit

---
+++ SpecFlow.MSTest.TestContext/SpecFlow.MSTest.Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpecFlow.MSTest.Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpecFlow.MSTest.Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("83c44131-07e7-46ce-8a70-1f179cf628d8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SpecFlow.MSTest.TestContext/SpecFlow.MSTest.Testing/TestContextExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpecFlow.MSTest.Testing
+{
+    public static class TestContextExtensionMethods
+    {
+        public static string GetTestRunParameterValue(this TestContext context, string key)
+        {
+            return context.Properties[key].ToString();
+        }
+    }
+}
